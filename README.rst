5b3bba58c3fede66152e56c9c0782daa817be89b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-05-25 08:08:44 -0400

    template

--- README.rst
+++ README.rst
@@ -1,3 +1,6 @@
+.. image:: https://travis-ci.org/scivision/starscale.svg?branch=master
+    :target: https://travis-ci.org/scivision/starscale
+
 =======================================
 Star Scalar (Photometry and Astrometry)
 =======================================
--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
-from setuptools import setup
+req = ['nose','sympy','scikit-image','astropy','matplotlib','h5py',]
+pipreq = ['photutils']
 
-req = ['nose','sympy','scikit-image','astropy','matplotlib','h5py',
-       'photutils']
+import pip
+try:
+    import conda.cli
+    conda.cli.main('install',*req)
+except Exception:
+    pip.main(['install'] + req)
+pip.main(['install'] +pipreq)
+# %%
+from setuptools import setup
 
 setup(name='starscale',
       packages=['starscale'],
       author='Michael Hirsch, Ph.D.',
       url = 'https://www.github.com/scivision/starscale',
-      install_requires=req,
+      install_requires=req+pipreq,
 	  )
 
